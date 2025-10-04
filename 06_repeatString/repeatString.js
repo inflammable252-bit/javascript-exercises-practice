f4435e5bd2f6fe3607b5d3b427d4797f70924ff3
inflammable252 <221427704+inflammable252-bit@users.noreply.github.com>
2025-10-04 15:39:41 -0700

    Complete exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = (string, num) => {
+    if (num < 0) return "ERROR";
+    let newString = "";
+    for (i=0; i < num; i++) {
+    newString += string;
+    }
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
