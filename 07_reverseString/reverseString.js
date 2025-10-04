8f986128ddce001bf67bbf2767660e0d2f1b3ac8
inflammable252 <221427704+inflammable252-bit@users.noreply.github.com>
2025-10-04 16:15:51 -0700

    Complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = (word) => {
+    let reversedWord = "";
+    let length = word.length;
+    for (i=word.length; i >= 0; i--) {
+        reversedWord += word.charAt(i);
+    }
+    return reversedWord;
 };
 
+/* const reverseString = (word) => (
+    word
+    .split("")
+    .reverse()
+    .join("")
+)
+ */
 // Do not edit below this line
 module.exports = reverseString;
