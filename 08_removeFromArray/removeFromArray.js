e308b7c59cf149b8d228819fb037ee99342db362
inflammable252 <221427704+inflammable252-bit@users.noreply.github.com>
2025-10-06 18:11:35 -0700

    Complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (arr, ...param) => {
+ const newArray = [];
+ arr.forEach(item => {
+    if (!param.includes(item)) {
+        newArray.push(item)
+    }
+ })
+ return newArray;  
+}
+
+// return arr.filter(val => !param.includes(val))
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
