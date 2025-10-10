7b6703f80692dc574688f2993b705f6dec5e2733
inflammable252 <221427704+inflammable252-bit@users.noreply.github.com>
2025-10-09 17:13:47 -0700

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
-};
+const sumAll = (x, y) => {
+    const input = [x, y];
+    for (const number of input) {
+        if (isNaN(number) || !Number.isInteger(number) || number < 0) {
+            return ("ERROR")
+        }
+    }
+    {
+        let array = input.sort((a, b) => a - b);
+        function spread(arr) {
+            const arrDiff = (arr[1] - arr[0])
+            let newArr = [];
+            i = 0;
+            while (newArr.length <= arrDiff) {
+                i++;
+                newArr.push((array[0] + i) - 1)
+            }
+            return newArr
+        }
+        return spread(array).reduce((total, curr) => total + curr)
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
