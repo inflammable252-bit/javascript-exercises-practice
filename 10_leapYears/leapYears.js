2374a012d142f055520151546d30354b4b0dbc1c
inflammable252 <221427704+inflammable252-bit@users.noreply.github.com>
2025-10-10 14:10:05 -0700

    Complete exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = (year) => {
+    // const isYearDivisibleByFour = year % 4 === 0;
+    // const isCentury = year % 100 === 0;
+    // const isYearDivisibleeByFourHundred = year % 400 === 0;
 
-};
+    //     if (isYearDivisibleByFour && (!isCentury || isYearDivisibleeByFourHundred)) {
+    //     return true;
+    //     }
+    // return false;
+    // }
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+    if (year % 4 == 0) {
+        return true;
+    }
+    return false;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
