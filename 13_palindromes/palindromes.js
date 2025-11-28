ec0d0adc308facf7d2f9ac60d0258b4493e7a64c
MT <221427704+inflammable252-bit@users.noreply.github.com>
2025-11-28 04:21:38 -0800

    Add draft of palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,40 @@
-const palindromes = function () {
+palindromes = function (input) {
+    inputArray = input.toLowerCase().split("");
+    filterItems = ["!", ".", ",", " "]
 
-};
+    function applyFilter(array) {
+        for (const fItem of filterItems) {
+            for (i = 0; i < array.length; i++) {
+                if (array[i] == fItem) {
+                    inputArray.splice(i, 1);
+                    i++;
+                }
+            }
+        }
+        return array
+    }
+    console.log(applyFilter(inputArray))
+    length = inputArray.length;
+
+    let firstHalf = [];
+    for (i = 1; i <= length / 2; i++) {
+        firstHalf.push(inputArray[i - 1])
+    }
+    let secondHalf = [];
+    for (i = inputArray.length; i > length / 2; i--) {
+        secondHalf.push(inputArray[i - 1])
+    }
+    if (length % 2 != 0) {
+        secondHalf.pop()
+    }
+
+    for (i = 0; i < (firstHalf.length - 1); i++) {
+        if (firstHalf[i] != secondHalf[i]) {
+            return false + " / " + firstHalf + " / " + secondHalf;
+        }
+        else return true + " / " + firstHalf + " / " + secondHalf;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
