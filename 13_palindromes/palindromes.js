86414675c0b9c46fa2e7043fd45ea321df9248c5
MT <221427704+inflammable252-bit@users.noreply.github.com>
2025-11-28 16:09:38 -0800

    Simplify logic for comparison Function no longer generates two arrays from the halves of the string to loop for comparison. Instead, a helper function takes inputArray and uses a single loop to increment index positions to be read and compared from either side of the word.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,40 +1,36 @@
 palindromes = function (input) {
-    inputArray = input.toLowerCase().split("");
-    filterItems = ["!", ".", ",", " "]
+inputArray = input.toLowerCase().split("");
+filterItems = ["!", ".", ",", " "]
 
-    function applyFilter(array) {
-        for (const fItem of filterItems) {
-            for (i = 0; i < array.length; i++) {
-                if (array[i] == fItem) {
-                    inputArray.splice(i, 1);
-                    i++;
-                }
+function applyFilter(array) {
+    for (const fItem of filterItems) {
+    for (i=0; i<array.length; i++) {
+            if (array[i] == fItem) {
+                inputArray.splice(i, 1);
+                i++;
             }
         }
-        return array
     }
-    console.log(applyFilter(inputArray))
-    length = inputArray.length;
-
-    let firstHalf = [];
-    for (i = 1; i <= length / 2; i++) {
-        firstHalf.push(inputArray[i - 1])
-    }
-    let secondHalf = [];
-    for (i = inputArray.length; i > length / 2; i--) {
-        secondHalf.push(inputArray[i - 1])
-    }
-    if (length % 2 != 0) {
-        secondHalf.pop()
-    }
-
-    for (i = 0; i < (firstHalf.length - 1); i++) {
-        if (firstHalf[i] != secondHalf[i]) {
-            return false + " / " + firstHalf + " / " + secondHalf;
-        }
-        else return true + " / " + firstHalf + " / " + secondHalf;
-    }
+    return array
 }
+console.log(applyFilter(inputArray))
+    function compare(array) {
+    let leftIndex=0;
+    let rightIndex=array.length-1;
 
+    while (leftIndex<array.length && rightIndex>=0) {
+        if (array[leftIndex] !== array[rightIndex]) {
+        // console.log(`${array[leftIndex]} and ${array[rightIndex]}`)
+        return false
+        }
+        else {
+            leftIndex++
+            rightIndex--
+        }
+        }
+    return true
+    }
+return compare(inputArray)
+}
 // Do not edit below this line
 module.exports = palindromes;
