62def8f7167b5388bd203dba44e14f0341a726e2
inflammable252 <221427704+inflammable252-bit@users.noreply.github.com>
2025-11-28 17:33:52 -0800

    Complete getTheTitles exercise Note: return .map is shorter than using .forEach to push into new array

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(array) {
+titleArray = []
+array.forEach((item) => {
+    titleArray.push(item.title);
+})
+return titleArray
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
