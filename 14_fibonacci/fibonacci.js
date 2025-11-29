713a307ec0f7a3d3e6967565a010245176c7c30b
inflammable252 <221427704+inflammable252-bit@users.noreply.github.com>
2025-11-28 17:03:36 -0800

    Complete fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+let fArray=[1];
+if (number<0 || typeof parseInt(number) != "number") {return "OOPS"}
+    for (i=0; i<number; i++) {
+        fArray.push((fArray[i-1] || 0) + fArray[i])
+    }
+return fArray[number-1] || 0
 };
 
 // Do not edit below this line
